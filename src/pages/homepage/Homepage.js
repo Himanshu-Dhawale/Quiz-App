443a3bc6da6dc7d25d65125f5b8dbe7162bd5d68
Himanshu <himanshudhawale9@gmail.com>
2022-07-15 10:50:36 +0530

    feat added homepage

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./homepage.css";
+import { Navigation } from '../../components/navigation/Navigation';
+import { Card } from '../../components/card/Card';
+import { Categories } from '../../database/category';
+const Homepage = () => {
+  return (
+    <div>
+        <Navigation/>
+        <main className="homepage__main">
+        <h1 className = "main__heading">⚡ Choose Category ⚡</h1>
+        <div className="space__2rem"></div>
+        <section className="quiz__card-section">
+            {Categories.map((items) => {
+                return(
+                    <Card items = {items} />
+                
+                )
+              
+            })}
+        </section>
+      </main>
+    </div>
+  )
+}
+
+export  {Homepage}
